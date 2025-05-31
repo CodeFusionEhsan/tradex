2fa0ce4178b8e92515abef6ac02e6570b548f52e
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-05-31 17:35:55 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://ehsan:ehsan2024@cluster0.vqrb8yl.mongodb.net/Tradex?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGO_URI;
 
 if (!MONGODB_URI) {
   throw new Error('MONGODB_URI must be defined in your .env.local file');
